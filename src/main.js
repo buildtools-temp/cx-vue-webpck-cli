8542149b20b4344f7a9bc8d263e5e926bf35e85f
xilanhuadechuntian <Lensgcx@163.com>
2018-12-14 16:10:59 +0800

    main
    main

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.less';
+
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/index.scss'
 import fetch from './fetch/index'
@@ -11,6 +14,10 @@ import fetch from './fetch/index'
 Vue.prototype.$fetch = fetch;
 Vue.config.productionTip = false;
 
+Vue.use(Vant);
+
+
+import './assets/css/test.less'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
