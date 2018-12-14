20e06e6d5b77ec333d52acc5b7a6bb3d8952c4bb
xilanhuadechuntian <Lensgcx@163.com>
2018-12-14 16:11:10 +0800

    App
    App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <!--<router-view/>-->
-    <h1 @click="login()">登录</h1>
-    <h1 @click="clickGET()">测试GET</h1>
-    <h1 @click="clickPOST()">测试POST</h1>
-    <h1 class="testh1">testh1</h1>
+    <img src="./assets/img/logo.png">
+    <router-view/>
+    <div style="margin-top: 30px">
+      <van-button type="default" @click="login()">登录</van-button>
+      <van-button type="primary" @click="clickGET()">测试GET</van-button>
+      <van-button type="warning" @click="clickPOST()">测试POST</van-button>
+      <van-button type="danger">other</van-button>
+    </div>
   </div>
 </template>
 
@@ -17,16 +19,11 @@
   export default {
     name: 'App',
     created() {
-      let a = [1, 2, 3];
-      let b = [1, 2, 3];
-      console.log([...a, ...b]);
-
       const InsertService = new insertService();
       InsertService.insert();
     },
     methods: {
       login() {
-        console.log('login');
         this.$fetch({
             url: '/oauth/login',
             method: 'post',
