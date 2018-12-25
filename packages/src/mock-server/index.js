02bc2b19b5cd938bcfbd9a4e35907265a3901ad8
xilanhuadechuntian <Lensgcx@163.com>
2018-12-25 14:54:13 +0800

    mock-server
    mock-server

---
+++ packages/src/mock-server/index.js
@@ -0,0 +1,15 @@
+const Mock = require('mockjs');
+const utils = require('../utils');
+const bodyParser = require('body-parser');
+const apiConfig = require(utils.rootPath('./mock/index'));
+
+module.exports = function (app) {
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  apiConfig.api.forEach(_conf => {
+    app[_conf.method](_conf.path, _conf.callback ? _conf.callback : function (rep, res) {
+      var json = utils.getJsonFile(_conf.dataFile);
+      res.json(Mock.mock(json));
+    })
+  });
+};
