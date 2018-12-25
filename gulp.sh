299f4cbb3d898d722a321b04a070210951b97031
xilanhuadechuntian <Lensgcx@163.com>
2018-12-25 14:51:37 +0800

    gulp sh

---
+++ gulp.sh
@@ -0,0 +1,15 @@
+#Design by Broccoli spring( gcx-高仓雄 ) <Lensgcx@163.com>
+#!/usr/bin/env bash
+
+ echo '========================== *** Packages run *** =========================='
+    echo
+    echo "Start time : `date +%Y-%m-%d,%H:%m:%s`"
+    echo
+
+      cd packages
+      gulp
+
+    echo
+    echo "End time : `date +%Y-%m-%d,%H:%m:%s`"
+    echo
+ echo '========================== *** Gulp run successful *** ===================='
