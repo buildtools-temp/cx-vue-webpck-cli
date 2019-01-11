5e740f3c7388a32665e5ad3066d5764f1e01b32e
xilanhuadechuntian <Lensgcx@163.com>
2019-01-11 17:24:29 +0800

    mock

--- docs/mock/index.js
+++ docs/mock/index.js
@@ -7,7 +7,8 @@ module.exports = {
     {id: "1", method: 'post', path: '/test/post', dataFile: "post.json", describe: "post-test"},
     {id: "2", method: 'get', path: '/test/get', dataFile: "get.json", describe: "get-test"},
     {
-      id: "3", method: 'post', path: '/oauth/login', dataFile: "login.json", callback: (rep, res) => {
+      id: "3", method: 'post', path: '/oauth/login', dataFile: "login.json",
+      callback: (rep, res) => {
         const fileMap = {
           0: "login_male.json",
           1: "login_female.json",
